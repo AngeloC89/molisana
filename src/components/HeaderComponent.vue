311b94cf06177c8dcf1979e918eb20659890ad08
AngeloC89 <Angelociulla89@gmail.com>
2024-04-10 13:29:52 +0200

    I made header and footer components

---
+++ src/components/HeaderComponent.vue
@@ -0,0 +1,41 @@
+<template>
+    <header class="container text-center ">
+        <img src="/images/la-molisana-logo.png" alt="La Molisana logo" />
+
+        <ul>
+            <li class="list-unstyled " v-for="item in menu" :key="item.name">
+                <a :href="item.url">{{ item.name }}</a>
+            </li>
+        </ul>
+    </header>
+</template>
+
+<script>
+    export default {
+        name: 'HeaderComponent',
+        data() {
+            return {
+                menu: [
+                    { name: 'Home', url: '#' },
+                    { name: 'Prodotti', url: '#' }, 
+                    { name: 'Chi siamo', url: '#' }, 
+                    { name: 'Contatti', url: '#' }, 
+                ],
+            };
+        },
+    };
+</script>
+
+<style lang="scss" scoped>
+ul{
+    display: flex;
+    justify-content: center;
+
+    li{
+        margin: 0 10px;
+        list-style: none;
+    }
+}
+
+    /* Add your styles here */
+</style>
---
+++ src/components/MainComponent.vue
@@ -0,0 +1,21 @@
+<template>
+    <main>
+<h2 class="text-center"> >>>goes content here<<< </h2>
+    </main>
+</template>
+
+<script>
+    export default {
+        name:'MainComponent'        
+    }
+</script>
+
+<style lang="scss" scoped>
+main{
+    background-image: url('/images/fondo-pag-speciali.jpg');
+    background-size: cover;
+
+
+}
+
+</style>
